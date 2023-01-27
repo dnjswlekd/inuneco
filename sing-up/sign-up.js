ecc2654d5355ce49986a5857466fee77d52cd5e5
dnjswlekd <wj8014@gmail.com>
2023-01-27 19:19:54 +0900

    finalProject(1)
    project

---
+++ sing-up/sign-up.js
@@ -0,0 +1,91 @@
+
+// select-dog or cat
+function change_img(e) {
+  var btns = document.querySelectorAll(".scale-img");
+  btns.forEach(function (img, i) {
+    if (e.currentTarget == img) {
+      img.classList.add("click-img");
+    } else {
+      img.classList.remove("click-img");
+    }
+  });
+  console.log(e.currentTarget);
+}
+
+// btn-animation
+const first = document.querySelector('#first-qna');
+const second = document.querySelector('#second-qna');
+const third = document.querySelector('#third-qna');
+const final = document.querySelector('#final');
+
+
+
+function ani() {
+    second.style.WebkitAnimation = "fadeIn 1s"; //css로
+        first.style.display = "none";
+        second.style.display = "block";
+        third.style.display = "none";
+        final.style.display = "none";
+}
+
+function ani2() {
+    third.style.WebkitAnimation = "fadeIn 1s";
+        first.style.display = "none";
+        second.style.display = "none";
+        third.style.display = "block";
+        final.style.display = "none";
+}
+
+function ani3() {
+    final.style.WebkitAnimation = "fadeIn 1s";
+        first.style.display = "none";
+        second.style.display = "none";
+        third.style.display = "none";
+        final.style.display = "block";
+}
+
+
+function gologin() {
+  addEventListener('click', e => {
+    window.location= 'http://127.0.0.1:5500/sign-in/login.html';
+  })
+}
+
+
+// forEach 사용해서 반복문 만들어보기
+
+  
+
+
+
+
+
+
+// progressbar
+// const progress = document.getElementById("progress")
+// const next = document.getElementById("next")
+// const circles = document.getElementById("btn")
+
+// let currentActive = 1;
+
+// next.addEventListener('click', ()=> {
+//   currentActive++;
+  
+//   if(currentActive > circles.length) {
+//     currentActive = circles.lenght;
+//   }
+//   update();
+// });
+
+// function update() {
+//   circles.forEach((circle, idx) => {
+//     if (idx < currentActive) {
+//       circle.classList.add("active");
+//     } else {
+//       circle.classList.remove("active");
+//     }
+//   });
+//   const actives = document.querySelectorAll('.active')
+
+// }
+
---
+++ sing-up/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    speed: 500,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.Q1-btn',
+    }
+  });
