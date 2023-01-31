1309be75d0df177a394ea141c064f496546328e2
dnjswlekd <wj8014@gmail.com>
2023-01-31 13:53:56 +0900

    수정3

--- sing-up/sign-up.js
+++ sing-up/sign-up.js
@@ -1,4 +1,3 @@
-
 // select-dog or cat
 function change_img(e) {
   var btns = document.querySelectorAll(".scale-img");
@@ -13,52 +12,42 @@ function change_img(e) {
 }
 
 // btn-animation
-const first = document.querySelector('#first-qna');
-const second = document.querySelector('#second-qna');
-const third = document.querySelector('#third-qna');
-const final = document.querySelector('#final');
-
-
+const first = document.querySelector("#first-qna");
+const second = document.querySelector("#second-qna");
+const third = document.querySelector("#third-qna");
+const final = document.querySelector("#final");
 
 function ani() {
-    second.style.WebkitAnimation = "fadeIn 1s"; //css로
-        first.style.display = "none";
-        second.style.display = "block";
-        third.style.display = "none";
-        final.style.display = "none";
+  second.style.WebkitAnimation = "fadeIn 1s"; //css로
+  first.style.display = "none";
+  second.style.display = "block";
+  third.style.display = "none";
+  final.style.display = "none";
 }
 
 function ani2() {
-    third.style.WebkitAnimation = "fadeIn 1s";
-        first.style.display = "none";
-        second.style.display = "none";
-        third.style.display = "block";
-        final.style.display = "none";
+  third.style.WebkitAnimation = "fadeIn 1s";
+  first.style.display = "none";
+  second.style.display = "none";
+  third.style.display = "block";
+  final.style.display = "none";
 }
 
 function ani3() {
-    final.style.WebkitAnimation = "fadeIn 1s";
-        first.style.display = "none";
-        second.style.display = "none";
-        third.style.display = "none";
-        final.style.display = "block";
+  final.style.WebkitAnimation = "fadeIn 1s";
+  first.style.display = "none";
+  second.style.display = "none";
+  third.style.display = "none";
+  final.style.display = "block";
 }
 
-
 function gofinal() {
-  addEventListener('click', e=> {
-    window.location = 'http://127.0.0.1:5500/sing-up/sing-up-final.html'
-  })
+  addEventListener("click", (e) => {
+    window.location = "sing-up-final.html";
+  });
 }
 
 // forEach 사용해서 반복문 만들어보기
-
-  
-
-
-
-
-
 
 // progressbar
 // const progress = document.getElementById("progress")
@@ -69,7 +58,7 @@ function gofinal() {
 
 // next.addEventListener('click', ()=> {
 //   currentActive++;
-  
+
 //   if(currentActive > circles.length) {
 //     currentActive = circles.lenght;
 //   }
@@ -87,4 +76,3 @@ function gofinal() {
 //   const actives = document.querySelectorAll('.active')
 
 // }
-
